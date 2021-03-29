bdc62346015327a7ead330b0febe096b8b92ad0b
Dimasik <dimatresko@gmail.com>
2021-03-29 10:50:51 +0300

    Small styles changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1 className="header__title">Best Games Market</h1>
-        <nav className="header__navigation">
+        <nav className="header__navbar">
           <NavLink to="/" exact className="header__link" activeClassName="header__link--active">Home</NavLink>
           <NavLink to="/products" className="header__link" activeClassName="header__link--active">Products</NavLink>
           <NavLink to="/about" className="header__link" activeClassName="header__link--active">About</NavLink>
