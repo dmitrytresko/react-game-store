269320948cc4657d2b81a4c636c41c8c5ef9fa0e
Dmitry-Tresko <dimatresko@gmail.com>
2021-03-30 14:02:35 +0300

    Adopted and beautified all pages

--- src/pages/CategoryPg/CategoryPg.jsx
+++ src/pages/CategoryPg/CategoryPg.jsx
@@ -1,10 +1,19 @@
 import { useParams } from "react-router-dom";
+import playStationLogo from "../../assets/img/playstation.png";
+import xboxLogo from "../../assets/img/xbox.png";
+import windowsLogo from "../../assets/img/windows.png";
+import "./styles.scss";
 
 const CategoryPg = () => {
   let { categoryId } = useParams();
 
+  console.log(categoryId);
+
   return (
-    <h2 className="page-title">Best games for {categoryId}</h2>
+    <div className="categories">
+      <img className="categories__logo" src={categoryId === "ps" ? playStationLogo : categoryId === "xbox" ? xboxLogo : windowsLogo}/>
+      <h2 className="page-title">- Best games for {categoryId} -</h2>
+    </div>
   )
 }
 
