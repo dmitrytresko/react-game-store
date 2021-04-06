f62eefaf2ece330cb55ecd8ad42419176af589ca
Dmitry-Tresko <dimatresko@gmail.com>
2021-04-06 10:09:55 +0300

    Form validation in progress

--- src/elements/Modal/Modal.jsx
+++ src/elements/Modal/Modal.jsx
@@ -1,29 +1,43 @@
 // import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import closeImg from "../../assets/img/close.png"
+import closeImg from "../../assets/img/close.png";
+import registrationSchema from "../../validations/userRegValidation";
 import "./styles.scss";
 
 const Modal = ({ opened, type, children, onCloseClick }) => {
+  const registerUser = async (event) => {
+    event.preventDefault();
+
+    let formData = {
+      login: event.target[1].value,
+      password: event.target[2].value,
+      doubledPassword: event.target[3].value
+    }
+
+    const isDataValid = await registrationSchema.isValid(formData);
+    console.log(isDataValid);
+  }
+
   if (!opened) return null;
 
   return ReactDOM.createPortal(
     <>
       <div className="content-overlay" />
 
-      <div className="modal">
+      <form className="modal" onSubmit={registerUser}>
         <div className="modal__head-block">
           <h2 className="modal__title">{type}</h2>
           <button className="modal__close-btn" type="button" onClick={onCloseClick}>
-            <img src={closeImg}></img>
+            <img src={closeImg} className="modal__close-btn--img"></img>
           </button>
         </div>
 
         {children}
 
         <div className="modal__bottom-block">
-          <button className="modal__submit-btn" type="submit" onClick={onCloseClick} >Submit</button>
+          <button className="modal__submit-btn" type="submit">Submit</button>
         </div>
-      </div>
+      </form>
     </>,
     document.getElementById('portal')
   )
