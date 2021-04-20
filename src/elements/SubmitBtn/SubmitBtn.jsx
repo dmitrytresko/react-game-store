0dd30e03566079b5a3dde5900413636087900245
Dmitry-Tresko <dimatresko@gmail.com>
2021-04-20 19:57:29 +0300

    Bugs regarding profile fixed

--- src/elements/SubmitBtn/SubmitBtn.jsx
+++
@@ -1,9 +0,0 @@
-import "./styles.scss";
-
-const SubmitBtn = ({ children }) => {
-  return (
-    <button className="submit-btn" type="submit">{children}</button>
-  )
-}
-
-export default SubmitBtn;
